1fabfedf06c4f1137b8c59a5420defa6ec1ae2ec
bunnysunny24 <bhavashesh@gmail.com>
2025-02-12 22:19:18 +0530

    ai lab

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Algo from './components/algo'; // Import the Algo component
+
+function App() {
+  return (
+    <div className="App">
+      <Algo /> {/* Render the Algo component */}
+    </div>
+  );
+}
+
+export default App;
